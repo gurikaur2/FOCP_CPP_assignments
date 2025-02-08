23fd5ed74b9513c528fdf6adba9f3d3e6b0bf9cb
gurikaur2 <gurnoorkaur.mail@gmail.com>
2025-02-08 21:53:56 +0530

    Initial commit

--- ASSIGNMENT 1/Q4.cpp
+++ ASSIGNMENT 1/Q4.cpp
@@ -1,1 +1,23 @@
 /* WAP that prints a spiral pattern of numbers for a given size of n */
+
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+    int n,x;
+    cout<<"Enter size of matrix:";
+    cin>>n;
+     for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            x=n*i+j+1;
+            cout<<x<<"  ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
